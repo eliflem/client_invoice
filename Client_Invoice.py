8658f22e5a0a03ee2acad4cf91b3ab105bcce34a
eliflem <70978219+eliflem@users.noreply.github.com>
2020-11-24 09:57:50 +0300

    Update Client_Invoice.py

--- Client_Invoice.py
+++ Client_Invoice.py
@@ -14,7 +14,7 @@ import datetime
 # In[2]:
 
 
-st.title("Homemade Invoice Generator - Individual")
+st.title("Homemade Invoice Generator")
 
 
 # In[3]:
@@ -22,31 +22,19 @@ st.title("Homemade Invoice Generator - Individual")
 
 user_name = st.sidebar.text_input("Please enter database user name")
 password = st.sidebar.text_input("Please enter database passsword", type="password")
-today = datetime.date.today()
-tomorrow = today + datetime.timedelta(days=1)
-start_date = st.sidebar.date_input('Start date', today)
-end_date = st.sidebar.date_input('End date', tomorrow)
-if start_date < end_date:
-    st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
-else:
-    st.error('Error: End date must fall after start date.')
+
 
 
 # In[ ]:
 
 
-disp_id = st.text_input("Please enter dispatcher client id")
+
 parasut_login = st.sidebar.text_input("Please enter Paraşüt username")
 parasut_pass = st.sidebar.text_input("Please enter Paraşüt password", type="password")
 parasaut_client_id = st.sidebar.text_input("Please enter Paraşüt client id", type="password")
 
 
 # In[ ]:
-
-
-start = start_date.strftime("%Y%m%d")
-end = end_date.strftime("%Y%m%d")
-today = today.strftime("%Y%m%d")
 
 
 # In[ ]:
@@ -85,11 +73,199 @@ headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json
 
 
 # In[ ]:
+tool_mode = st.selectbox("Select tool", ["Please select from the list", "Individual Invoice", "Bulk Invoice")
+                                         
+if tool_mode == "Individual Invoice":
+    st.write("This tool creates a single invoice for the given client based on the completed orders between selected dates")
+    disp_id = st.text_input("Please enter dispatcher client id")
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    start_date = st.date_input('Start date', today)
+    end_date = st.date_input('End date', tomorrow)
+    if start_date < end_date:
+        st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+    else:
+        st.error('Error: End date must fall after start date.')                                         
+
+    start = start_date.strftime("%Y%m%d")
+    end = end_date.strftime("%Y%m%d")
+    today = today.strftime("%Y%m%d")     
+                                         
+    mycursor = mydb.cursor(dictionary=True)
+
+    sql = "SELECT o.user_id, sum(o.payment) as payment, l.bank_name as parasut_id, l.address_legal_city as vergi_no, l.address_legal as address, l.address_actual as vergi_dairesi from orders o left join legal_persons l on l.user_id=o.user_id WHERE o.status_id='3' and o.finish_datetime between '"+start+"' and '"+end+"' and o.user_id='"+disp_id+"' group by user_id"
+
+    mycursor.execute(sql)
+
+    myresult = mycursor.fetchall()
+
+    for row in myresult:
+        row["payment"] = float(row["payment"]*100/118)
+        row["user_id"] = str(row["user_id"])
+        row["vergi_no"] = str(row["vergi_no"])
+        row["address"] = str(row["address"])
+        row["vergi_dairesi"] = str(row["vergi_dairesi"])
 
 
-mycursor = mydb.cursor(dictionary=True)
+    # In[ ]:
 
-sql = "SELECT o.user_id, sum(o.payment) as payment, l.bank_name as parasut_id, l.address_legal_city as vergi_no, l.address_legal as address, l.address_actual as vergi_dairesi from orders o left join legal_persons l on l.user_id=o.user_id WHERE o.status_id='3' and o.finish_datetime between '"+start+"' and '"+end+"' and o.user_id='"+disp_id+"' group by user_id"
+
+    def order_list(client_id):
+        mycursor_2 = mydb.cursor()
+        query = "SELECT order_id from orders WHERE user_id='"+client_id+"' and status_id='3' and finish_datetime between '"+start+"' and '"+end+"'"
+        mycursor_2.execute(query)
+        result = mycursor_2.fetchall()
+        orders = [list(i) for i in result]
+        orders_str = ' '.join([str(element) for element in orders]) 
+        return(orders_str)
+
+
+    # In[ ]:
+
+
+    def product(client_id):
+        #create product
+            product = {
+                "data": {
+                "id": "",
+                "type": "products",
+                "attributes": {
+                "code": "",
+                "name": "Fatura döneminde tamamlanmış gönderiler: "+order_list(client_id),
+                "unit:": 1,
+                "vat_rate": 18.0,
+                "sales_excise_duty": 0,
+                "unit": "",
+                "communications_tax_rate": 0,
+                "list_price": 0,
+                "list_price_in_trl": 0,
+                "currency": "TRL",
+                "buying_price": 0,
+                "initial_stock_count": 0
+                },
+                "relationships": {
+                }
+                }
+                }
+            prod = r.post("https://api.parasut.com/v4/183239/products", headers=headers, json=product)
+            prod_json = prod.json()
+            product_id = prod_json["data"]["id"]
+            return(product_id)
+
+
+
+    # In[ ]:
+
+
+    for row in myresult:
+        row["order_ids"] = order_list(row["user_id"])
+        row["product_id"] = product(row["user_id"])
+
+
+    # In[ ]:
+
+
+    for row in myresult:
+        invoice = {
+                "data": {
+                "id": "",
+                "type": "sales_invoices",
+                "attributes": {
+                "item_type": "invoice",
+                "description": "Hizmet Bedeli",
+                "issue_date": today,
+                "due_date": today,
+                "invoice_series": "",
+                "currency": "TRL",
+                "exchange_rate": 0,
+                "withholding_rate": 0,
+                "vat_withholding_rate": 0,
+                "invoice_discount_type": "percentage",
+                "invoice_discount": 0,
+                "billing_address": row["address"],
+                "billing_phone": "",
+                "billing_fax": "",
+                "tax_office": row["vergi_dairesi"],
+                "tax_number": row["vergi_no"],
+                "city": "",
+                "district": "",
+                "is_abroad": "",
+                "order_no": "",
+                "order_date": today,
+                "shipment_addres": "",
+                },
+                "relationships": {
+                "details": {
+                "data": [
+                  {
+                    "id": "",
+                    "type": "sales_invoice_details",
+                    "attributes": {
+                      "quantity": 1,
+                      "unit_price": row["payment"],
+                      "vat_rate": 18.0,
+                      "discount_type": "percentage",
+                      "discount_value": 0,
+                      "excise_duty_type": "percentage",
+                      "excise_duty_value": 0,
+                      "communications_tax_rate": 0,
+                      "description": ""
+                    },
+                    "relationships": {
+                      "product": {
+                        "data": {
+                          "id": row["product_id"],
+                          "type": "products"
+                        }
+                      }
+                    }
+                  }
+                ]
+                },
+                "contact": {
+                "data": {
+                  "id": row["parasut_id"],
+                  "type": "contacts"
+                }
+                },
+                "category": {
+                "data": {
+                  "id": "4901641",
+                  "type": "item_categories"
+                }
+                },
+                "sales_offer": {
+                "data": {
+                  "id": "",
+                  "type": "sales_offers"
+                }
+                }
+                }
+                }
+                }
+        inv = r.post("https://api.parasut.com/v4/183239/sales_invoices", headers=headers, json=invoice)
+        inv_json = inv.json()
+        st.write(row["user_id"], inv_json["data"]["id"])
+
+                                        
+elif tool_mode == "Bulk Invoice":
+    st.write("This tool creates seperate invoices for each balance client who completed at least one order between the given dates")                                             
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    start_date = st.date_input('Start date', today)
+    end_date = st.date_input('End date', tomorrow)
+    if start_date < end_date:
+        st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+    else:
+        st.error('Error: End date must fall after start date.')
+                                         
+    start = start_date.strftime("%Y%m%d")
+    end = end_date.strftime("%Y%m%d")
+    today = today.strftime("%Y%m%d")     
+                                         
+    mycursor = mydb.cursor(dictionary=True)
+
+sql = "SELECT o.user_id, sum(o.payment) as payment, l.bank_name as parasut_id, l.address_legal_city as vergi_no, l.address_legal as address, l.address_actual as vergi_dairesi from orders o left join legal_persons l on l.user_id=o.user_id WHERE o.status_id='3' and o.finish_datetime between '"+start+"' and '"+end+"' group by user_id"
 
 mycursor.execute(sql)
 
@@ -241,5 +417,5 @@ for row in myresult:
             }
     inv = r.post("https://api.parasut.com/v4/183239/sales_invoices", headers=headers, json=invoice)
     inv_json = inv.json()
-    st.write(row["user_id"], inv_json["data"]["id"])
-
+    st.write(row["user_id"], inv_json["data"]["id"])                                    
+                                         
